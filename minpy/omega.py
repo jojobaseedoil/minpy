d5ef2915a55695dc83cef24db12521f940d2dd8e
jojobaseedoil <jojobaseedoil@gmail.com>
2024-06-06 21:47:02 -0300

    optimizing route function

--- minpy/omega.py
+++ minpy/omega.py
@@ -2,6 +2,7 @@ from math import ceil, pow
 import numpy as np
 from numba import jit
 from .multistage import Multistage
+from concurrent.futures import ThreadPoolExecutor
 from .slider import Slider
 
 class Omega(Multistage):
@@ -22,7 +23,6 @@ class Omega(Multistage):
         self.__WINDOW      : Slider = Slider(size, extras, radix)
         self.__EXTRA_CODES : list   = [i for i in range(ceil(pow(radix, extras)))]
 
-    @jit
     def route(self, input: int, output: int) -> bool:
         """
         Routes a message from input to output.
@@ -34,7 +34,7 @@ class Omega(Multistage):
         Returns:
             bool: True if routing is successful, False otherwise.
         """
-        assert 0 <= input  < len(self), f"Index {input} out of range."
+        assert 0 <= input < len(self), f"Index {input} out of range."
         assert 0 <= output < len(self), f"Index {output} out of range."
 
         if output in self._routed:
@@ -42,30 +42,16 @@ class Omega(Multistage):
 
         paths = [self.__WINDOW.concat(input, extra, output) for extra in self.__EXTRA_CODES]
 
-        for path in paths:
-            if self.__is_path_available(path):
+        with ThreadPoolExecutor() as executor:
+            results = executor.map(self.__is_path_available, paths)
+
+        for path, is_available in zip(paths, results):
+            if is_available:
                 self._routed[output] = path
                 self.__send_message(path)
                 return True
 
         return False
-    
-    @jit
-    def __is_path_available(self, path: int) -> bool:
-        """
-        Checks if a path is available for the given path.
-
-        Args:
-            path (int): Binary Path.
-
-        Returns:
-            bool: True if the path is available, False otherwise.
-        """
-        for col in range(self.stages):
-            row = self.__WINDOW.slide(path, col + 1)
-            if not self.__is_available(path, row, col):
-                return False
-        return True
 
     def unroute(self, output: int) -> bool:
         """
@@ -97,7 +83,24 @@ class Omega(Multistage):
         del self._routed[output]
 
         return True
+    
+    @jit
+    def __is_path_available(self, path: int) -> bool:
+        """
+        Checks if a path is available for the given path.
 
+        Args:
+            path (int): Binary Path.
+
+        Returns:
+            bool: True if the path is available, False otherwise.
+        """
+        for col in range(self.stages):
+            row = self.__WINDOW.slide(path, col + 1)
+            if not self.__is_available(path, row, col):
+                return False
+        return True
+    
     def __is_available(self, path: int, row: int, col: int) -> bool:
         """
         Checks if a path is available.
